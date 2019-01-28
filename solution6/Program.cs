9c12cf1a4c5b12fed021bc8e2d7aaa216eafa3fb
saksham rana <0blackbuck@gmail.com>
2019-01-27 23:06:29 -0500

    sol6

--- solution6/Program.cs
+++ solution6/Program.cs
@@ -6,7 +6,38 @@ namespace solution6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 1, 2, 3, 2, 2, 1, 3, 2 };//dataset given in example
+            computeFrequency(arr);//call method
+            Console.ReadLine();//hold the output screen
+        }
+        public static void computeFrequency(int[] a)
+        {
+            Console.WriteLine("Number  Frequency"); //to differentiate between value and frequency
+            try
+            {
+                int x = a.Length;
+                Boolean[] counted = new Boolean[x]; //array to check that number is counted         
+                for (int i = 0; i < x; i++)
+                {
+                    if (counted[i] == true)
+                        continue;    //continue if number is counted previously
+                    int freq = 1; // define freq to 1
+                    for (int j = i + 1; j < x; j++)  //count freq of a number in an array
+                    {
+                        if (a[i] == a[j]) //check if value is present in j as well
+                        {
+                            counted[j] = true;
+                            freq++;// increment freq by 1
+                        }
+
+                    }
+                    Console.WriteLine(a[i] + "       " + freq);//display number and their frequency
+                }
+            }
+            catch
+            {
+                Console.WriteLine("exception try again");//error message
+            }
         }
     }
 }
